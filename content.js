03208bda7bb0ac550a5c0b9623d00236f97fd0bd
guka jincharadze <115719203+jinchara@users.noreply.github.com>
2023-11-09 20:39:13 +0400

    Update content.js
    I returned on the first step

--- content.js
+++ content.js
@@ -3,46 +3,6 @@
 // Load your list of words to replace from your database
 const wordsToReplace = ['chess', 'example', 'anotherword','gave'];
 
-function getTextOfComment(){
-  const container = document.querySelectorAll('div[class="xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs"]');
-
-
-  const result = container.textContent.trim();
-
-  chrome.runtime.sendMessage({ text: result }); // Send the text to the background script
-
-}
-
-
-
-// content.js (Content Script)
-const targetElement = document.querySelectorAll('div[class="xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs"]');; // Replace 'your-selector' with the appropriate selector
-
-// Define a function to handle the visibility change
-function handleVisibilityChange(entries, observer) {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            // The element is visible
-            const text = entry.target.textContent;
-            chrome.runtime.sendMessage({ text: text });
-            observer.disconnect(); // Stop observing once the element is visible
-        }
-    });
-}
-
-// Create an Intersection Observer
-const observer = new IntersectionObserver(handleVisibilityChange, {
-    root: null, // Use the viewport as the root
-    threshold: 0.5, // Trigger when 50% of the element is visible
-});
-
-if (targetElement) {
-    observer.observe(targetElement);
-}
-
-
-
-
 // Function to replace the words from the database with spaces within <div> elements
 function replaceWordsWithSpacesInDivs() {
   const divElements = document.querySelectorAll('div[class="xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs"]');
